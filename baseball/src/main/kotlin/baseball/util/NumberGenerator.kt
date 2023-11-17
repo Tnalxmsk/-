298bec817251b2e7434c978e4c0fcfb806e760a8
Tnalxmsk <alstnwkd990@naver.com>
2023-11-17 13:28:50 +0900

    feat(baseball): 컴퓨터 숫자 생성 기능 추가

---
+++ baseball/src/main/kotlin/baseball/util/NumberGenerator.kt
@@ -0,0 +1,16 @@
+package baseball.util
+
+import camp.nextstep.edu.missionutils.Randoms
+
+object NumberGenerator {
+    fun generateNumbers(): List<Int> {
+        val computer = mutableListOf<Int>()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+        return computer
+    }
+}
