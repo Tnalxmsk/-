3d7677a65d7a6ec9b735ad79b14ca2cb3adb3f59
Tnalxmsk <alstnwkd990@naver.com>
2023-11-20 17:38:36 +0900

    feat(baseball): 게임 재시작 입력 기능 추가

--- baseball/src/main/kotlin/baseball/view/InputView.kt
+++ baseball/src/main/kotlin/baseball/view/InputView.kt
@@ -10,7 +10,16 @@ class InputView {
         return BallNumber(numbers.map { it.digitToInt() })
     }
 
+    fun readRestart() : Int {
+        println(ALL_CORRECT)
+        println(INPUT_RESTART)
+        val number = Console.readLine()
+        return number.toInt()
+    }
+
     companion object {
         private const val INPUT_VIEW = "숫자를 입력해주세요: "
+        private const val ALL_CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        private const val INPUT_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     }
 }
