f50f92b0a232f0577c096720b858d5e69872821c
Tnalxmsk <alstnwkd990@naver.com>
2023-11-20 21:41:00 +0900

    refactor(baseball): BaseballApp 실행 로직 수정

--- baseball/src/main/kotlin/baseball/BaseballApp.kt
+++ baseball/src/main/kotlin/baseball/BaseballApp.kt
@@ -1,9 +1,8 @@
 package baseball
 
-import baseball.data.BallNumber
-import baseball.data.BaseballResult
 import baseball.data.Computer
 import baseball.data.Player
+import baseball.extention.isAllStrike
 import baseball.util.NumberComparator
 import baseball.util.NumberGenerator
 import baseball.view.InputView
@@ -14,38 +13,23 @@ class BaseballApp(
     private val outputView: OutputView
 ) {
     fun runBaseballGame() {
-        outputView.printStartView()
+        outputView.printIntro()
         while (true) {
             controlGame()
             when(inputView.readRestart()) {
-                RESTART -> continue
-                STOP -> return
+                GameControl.CONTINUE_GAME.value -> continue
+                GameControl.STOP_GAME.value -> return
             }
         }
     }
 
-    private fun applyAnswerNumber() : Computer {
-        val answerNumber = BallNumber(NumberGenerator.generateNumbers())
-        return Computer(answerNumber)
-    }
-
     private fun controlGame() {
-        val computer = applyAnswerNumber()
+        val computer = Computer(NumberGenerator.generateNumbers())
         while (true) {
             val player = Player(inputView.readUserNumbers())
             val result = NumberComparator.compareNumbers(player.playerNumber, computer.computerNumber)
             outputView.printGameResult(result)
-            if (isAllStrike(result)) return
+            if (result.isAllStrike()) return
         }
-    }
-
-    private fun isAllStrike(result: BaseballResult): Boolean {
-        return result.strike == ALL_STRIKE
-    }
-
-    companion object {
-        private const val ALL_STRIKE = 3
-        private const val STOP = 2
-        private const val RESTART = 1
     }
 }
