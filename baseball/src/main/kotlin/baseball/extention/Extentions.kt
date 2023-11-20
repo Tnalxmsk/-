995ebd8a6817ac5cd922be586ab065cc160745ff
Tnalxmsk <alstnwkd990@naver.com>
2023-11-20 22:42:36 +0900

    feat(baseball): String을 Int List로 변환하는 확장함수 추가

--- baseball/src/main/kotlin/baseball/extention/Extentions.kt
+++ baseball/src/main/kotlin/baseball/extention/Extentions.kt
@@ -7,3 +7,7 @@ private const val ALL_STRIKE = 3
 fun BaseballResult.isAllStrike(): Boolean {
     return this.strike == ALL_STRIKE
 }
+
+fun String.toIntList(): List<Int> {
+    return this.map { it.digitToInt() }
+}
