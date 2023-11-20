b5fc25da4ae768b679588a09c9850f7bd3176025
Tnalxmsk <alstnwkd990@naver.com>
2023-11-20 16:33:01 +0900

    feat(baseball): 비교한 결과를 출력하는 기능 추가

--- baseball/src/main/kotlin/baseball/view/OutputView.kt
+++ baseball/src/main/kotlin/baseball/view/OutputView.kt
@@ -1,11 +1,25 @@
 package baseball.view
 
+import baseball.data.BaseballResult
+
 class OutputView {
     fun printStartView() {
         println(GAME_START_VIEW)
     }
 
+    fun printGameResult(result: BaseballResult) {
+        when {
+            result.ball == 0 && result.strike == 0 -> println(NOTHING)
+            result.ball == 0 -> println(STRIKE.format(result.strike))
+            result.strike == 0 -> println(BALL.format(result.ball))
+            else -> println("${BALL.format(result.ball)} ${STRIKE.format(result.strike)}")
+        }
+    }
+
     companion object {
         private const val GAME_START_VIEW = "숫자 야구 게임을 시작합니다."
+        private const val STRIKE = "%d스트라이크"
+        private const val BALL = "%d볼"
+        private const val NOTHING = "낫싱"
     }
 }
