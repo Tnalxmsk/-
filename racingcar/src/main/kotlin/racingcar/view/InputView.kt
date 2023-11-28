cb32e8c83f579bb773e167b6afe2f99560fe52bf
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 17:25:21 +0900

    feat(racingcar): 입력뷰 출력 문구 추가

--- racingcar/src/main/kotlin/racingcar/view/InputView.kt
+++ racingcar/src/main/kotlin/racingcar/view/InputView.kt
@@ -4,12 +4,19 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readCarName(): String {
+        println(CAR_NAME_INPUT_VIEW)
         val names = Console.readLine()
         return names
     }
 
     fun readTotalRound(): Int {
+        println(TOTAL_ROUND_INPUT_VIEW)
         val totalRound = Console.readLine()
         return totalRound.toInt()
+    }
+
+    companion object {
+        private const val CAR_NAME_INPUT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)"
+        private const val TOTAL_ROUND_INPUT_VIEW = "시도할 횟수는 몇 회인가요?"
     }
 }
