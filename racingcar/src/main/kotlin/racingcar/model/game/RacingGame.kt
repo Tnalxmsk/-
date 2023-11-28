3307981b36a3b398331797386d11aab9b28bca8c
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 17:26:08 +0900

    feat(racingcar): 라운드 별로 움직임을 결정하는 기능 추가

--- racingcar/src/main/kotlin/racingcar/model/game/RacingGame.kt
+++ racingcar/src/main/kotlin/racingcar/model/game/RacingGame.kt
@@ -3,6 +3,7 @@ package racingcar.model.game
 import racingcar.User
 import racingcar.extension.toCarList
 import racingcar.model.car.Car
+import racingcar.util.NumberGenerator
 import racingcar.view.InputView
 
 class RacingGame(
@@ -10,10 +11,25 @@ class RacingGame(
 ) : Game {
     override fun startGame() {
         val user = createUser()
+        val round = inputView.readTotalRound()
+        startRound(user)
     }
 
     private fun createUser(): User {
         val cars: List<Car> = inputView.readCarName().toCarList()
         return User(cars)
+    }
+
+    private fun startRound(user: User) {
+        decideCarMove(user)
+    }
+
+    private fun decideCarMove(user: User) {
+        user.cars.map { car ->
+            when {
+                NumberGenerator.getRandomNumber() < 4 -> car.stopMove()
+                else -> car.moveForward()
+            }
+        }
     }
 }
