a3e1298d4f90f8bfadd8e3345ae59d19c5909393
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 20:24:53 +0900

    feat(racingcar): 최종 우승자를 출력하는 기능 추 가

--- racingcar/src/main/kotlin/racingcar/view/OutputView.kt
+++ racingcar/src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,6 +1,8 @@
 package racingcar.view
 
+import racingcar.extension.getAllWinnerName
 import racingcar.model.User
+import racingcar.model.Winner
 
 class OutputView {
     fun printCarDistance(user: User) {
@@ -10,7 +12,12 @@ class OutputView {
         println()
     }
 
+    fun printGameWinner(winner: Winner) {
+        println(RACING_WINNER_OUTPUT_VIEW.format(winner.getAllWinnerName()))
+    }
+
     companion object {
         private const val CAR_DISTANCE_OUTPUT_VIEW = "%s : %s"
+        private const val RACING_WINNER_OUTPUT_VIEW = "최종 우승자 : %s"
     }
 }
