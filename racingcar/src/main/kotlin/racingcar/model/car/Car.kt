8544bbad65cd570b445c28958ce123c31032defe
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 21:46:04 +0900

    refactor(racingcar): 코드 개선
    Car class 일부 코드 상수화

--- racingcar/src/main/kotlin/racingcar/model/car/Car.kt
+++ racingcar/src/main/kotlin/racingcar/model/car/Car.kt
@@ -1,15 +1,20 @@
 package racingcar.model.car
 
 class Car(val name: String) : Movement {
-    private var distance = ""
+    private var distance: String = DEFAULT_DISTANCE
 
     fun getDistance() = distance
 
     override fun moveForward() {
-        distance += "-"
+        distance += PLUS_DISTANCE
     }
 
     override fun stopMove() {
         // 움직임을 멈추기 때문에 거리가 증가하지 않음
+    }
+
+    companion object {
+        private const val DEFAULT_DISTANCE = ""
+        private const val PLUS_DISTANCE = "-"
     }
 }
