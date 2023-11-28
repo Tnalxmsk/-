7bfbfda4c0d147033c701aafcdc574aa480734ab
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 21:54:25 +0900

    refactor(racingcar): 코드 개선
    매직 넘버 상수화

--- racingcar/src/main/kotlin/racingcar/util/NumberGenerator.kt
+++ racingcar/src/main/kotlin/racingcar/util/NumberGenerator.kt
@@ -3,7 +3,10 @@ package racingcar.util
 import camp.nextstep.edu.missionutils.Randoms
 
 object NumberGenerator {
+    private const val START_NUMBER = 0
+    private const val LAST_NUMBER = 0
+
     fun getRandomNumber(): Int {
-        return Randoms.pickNumberInRange(0, 9)
+        return Randoms.pickNumberInRange(START_NUMBER, LAST_NUMBER)
     }
 }
