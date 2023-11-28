f19d6b436577ac5f32fb977c7cd966eb88aaff33
Tnalxmsk <alstnwkd990@naver.com>
2023-11-28 23:15:29 +0900

    fix(racingcar): 0만 생성하던 버그 수정

--- racingcar/src/main/kotlin/racingcar/util/NumberGenerator.kt
+++ racingcar/src/main/kotlin/racingcar/util/NumberGenerator.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 object NumberGenerator {
     private const val START_NUMBER = 0
-    private const val LAST_NUMBER = 0
+    private const val LAST_NUMBER = 9
 
     fun getRandomNumber(): Int {
         return Randoms.pickNumberInRange(START_NUMBER, LAST_NUMBER)
