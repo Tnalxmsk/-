1f03374318fc039a3dc091f1dc1ae54d9ee33f57
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 19:51:09 +0900

    feat(lotto): 당첨 결과 enum class 추가

---
+++ lotto/src/main/kotlin/lotto/model/WinningResult.kt
@@ -0,0 +1,10 @@
+package lotto.model
+
+enum class WinningResult(val text: String, val prizeAmount: Int) {
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    THIRD("5개 일치 (1,500,000원)", 1_500_000),
+    FORTH("4개 일치 (50,000원)", 50_000),
+    FIFTH("3개 일치 (5,000원)", 5_000),
+    FAIL("꽝", 0)
+}
