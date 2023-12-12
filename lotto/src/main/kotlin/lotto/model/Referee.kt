4593253c7715119da4473ae663f6b6468a18e563
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 19:51:54 +0900

    feat(lotto): 당첨을 판단하는 기능 추가

--- lotto/src/main/kotlin/lotto/model/Referee.kt
+++ lotto/src/main/kotlin/lotto/model/Referee.kt
@@ -1,8 +1,25 @@
 package lotto.model
 
+import lotto.extension.isContainBonusNumber
+
 class Referee(
     private val winningNumber: Lotto,
     private val bonusNumber: Int
 ) {
+    private fun getSameNumbers(lotto: Lotto): List<Int> {
+        val userWinningNumbers = lotto.getLottoNumber().filter { winningNumber.getLottoNumber().contains(it) }
+        return userWinningNumbers
+    }
 
+    fun judeWinning(lotto: Lotto): WinningResult {
+        val size = getSameNumbers(lotto).size
+        return when {
+            size == 6 -> WinningResult.FIRST
+            size == 5 && lotto.isContainBonusNumber(bonusNumber) -> WinningResult.SECOND
+            size == 5 -> WinningResult.THIRD
+            size == 4 -> WinningResult.FORTH
+            size == 3 -> WinningResult.FIFTH
+            else -> WinningResult.FAIL
+        }
+    }
 }
