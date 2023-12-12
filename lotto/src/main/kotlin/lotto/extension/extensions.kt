302c76b130d545b16629e8ec54b805fd308e1d2a
Tnalxmsk <alstnwkd990@naver.com>
2023-12-13 00:04:37 +0900

    refactor(lotto): 로또 번호 형식 확인 확장함수 로직 수정

--- lotto/src/main/kotlin/lotto/extension/extensions.kt
+++ lotto/src/main/kotlin/lotto/extension/extensions.kt
@@ -1,14 +1,15 @@
 package lotto.extension
 
+import lotto.config.LottoRule
 import lotto.model.Lotto
 
 fun Lotto.isContainBonusNumber(bonusNumber: Int): Boolean {
     return this.getLottoNumber().contains(bonusNumber)
 }
 
-fun List<String>.isNotLottoNumberFormat(): Boolean {
+fun List<String>.isNotLottoNumberFormat(lottoRule: LottoRule): Boolean {
     this.forEach {
-        if (it.matches(Regex("\\d")).not() || (1..45).contains(it.toInt()).not()) {
+        if (it.matches(Regex("\\d+")).not() || it.toInt() !in lottoRule.minNumber..lottoRule.maxNumber) {
             return true
         }
     }
