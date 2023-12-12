35a4a35dad373d7a31b09103aa350f1bbfe6e13a
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 18:47:04 +0900

    refactor(lotto): 로또 세부 내역 출력 기능 분리
    로또 개수를 출력하는 메서드,
    로또 번호를 출력하는 메서드로 분리

--- lotto/src/main/kotlin/lotto/view/OutputView.kt
+++ lotto/src/main/kotlin/lotto/view/OutputView.kt
@@ -4,12 +4,18 @@ import lotto.model.Lotto
 
 class OutputView {
     fun printPurchaseDetails(lottoBundle: List<Lotto>) {
+        printPurchaseLottoSummary(lottoBundle)
+        printPurchaseLottoNumbers(lottoBundle)
+    }
+
+    private fun printPurchaseLottoSummary(lottoBundle: List<Lotto>) =
         println(VIEW_PURCHASE_DETAILS.format(lottoBundle.size))
-        lottoBundle.forEach {
-            println(it.getLottoNumber())
-        }
+
+    private fun printPurchaseLottoNumbers(lottoBundle: List<Lotto>) {
+        lottoBundle.forEach { println(it.getLottoNumber()) }
         println()
     }
+
 
     companion object {
         private const val VIEW_PURCHASE_DETAILS = "%d개를 구매했습니다."
