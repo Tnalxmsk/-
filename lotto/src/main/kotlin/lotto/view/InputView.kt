eb0d9cfb330c40a1d818d9dbf434d3ebb6af5de3
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 19:05:24 +0900

    feat(lotto): 당첨 번호, 보너스 숫자 입력 기능 추가

--- lotto/src/main/kotlin/lotto/view/InputView.kt
+++ lotto/src/main/kotlin/lotto/view/InputView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.model.Lotto
 
 class InputView {
     fun readAmount(): Int {
@@ -10,8 +11,21 @@ class InputView {
         return amount.toInt()
     }
 
+    fun readWinningNumber(): Lotto {
+        println(INPUT_WINNING_NUMBER)
+        val winningNumber = Console.readLine()
+        return Lotto(winningNumber.map { it.digitToInt() })
+    }
+
+    fun readBonusNumber(): Int {
+        println(INPUT_BONUS_NUMBER)
+        val bonusNumber = Console.readLine()
+        return bonusNumber.toInt()
+    }
 
     companion object {
         private const val INPUT_READ_AMOUNT = "구입금액을 입력해 주세요."
+        private const val INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+        private const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
     }
 }
