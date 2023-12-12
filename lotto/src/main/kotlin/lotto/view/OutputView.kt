9cff867917e5c7c8fd22988bd1414f66209ee95a
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 20:46:41 +0900

    feat(lotto): 당첨 통계 출력 기능 추가

--- lotto/src/main/kotlin/lotto/view/OutputView.kt
+++ lotto/src/main/kotlin/lotto/view/OutputView.kt
@@ -1,11 +1,11 @@
 package lotto.view
 
 import lotto.model.Lotto
+import lotto.model.WinningResult
 
 class OutputView {
     fun printPurchaseDetails(lottoBundle: List<Lotto>) {
         printPurchaseLottoSummary(lottoBundle)
-        println()
         printPurchaseLottoNumbers(lottoBundle)
         println()
     }
@@ -17,13 +17,23 @@ class OutputView {
         lottoBundle.forEach { println(it.getLottoNumber()) }
     }
 
+    fun printWinningDetails(result: List<WinningResult>) {
+        println(VIEW_DETAILS)
+        println("${WinningResult.FIFTH.text}${NUMBER_WINNING.format(result.count { it == WinningResult.FIFTH })}")
+        println("${WinningResult.FORTH.text}${NUMBER_WINNING.format(result.count { it == WinningResult.FORTH })}")
+        println("${WinningResult.THIRD.text}${NUMBER_WINNING.format(result.count { it == WinningResult.THIRD })}")
+        println("${WinningResult.SECOND.text}${NUMBER_WINNING.format(result.count { it == WinningResult.SECOND })}")
+        println("${WinningResult.FIRST.text}${NUMBER_WINNING.format(result.count { it == WinningResult.FIRST })}")
+    }
+
     fun printTotalProfit(profit: Double) {
         println(VIEW_TOTAL_PROFIT.format(profit))
     }
 
-
     companion object {
         private const val VIEW_PURCHASE_DETAILS = "%d개를 구매했습니다."
         private const val VIEW_TOTAL_PROFIT = "총 수익률은 %.1f%%입니다."
+        private const val VIEW_DETAILS = "당첨 통계\n---"
+        private const val NUMBER_WINNING = " - %d개"
     }
 }
