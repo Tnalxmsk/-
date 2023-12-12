ed8e2b1047f05800c3171ca5aff7e0c252824de9
Tnalxmsk <alstnwkd990@naver.com>
2023-12-12 20:16:00 +0900

    feat(lotto): 총 수익률 출력 기능 추가

--- lotto/src/main/kotlin/lotto/view/OutputView.kt
+++ lotto/src/main/kotlin/lotto/view/OutputView.kt
@@ -16,8 +16,13 @@ class OutputView {
         println()
     }
 
+    fun printTotalProfit(profit: Double) {
+        println(VIEW_TOTAL_PROFIT.format(profit))
+    }
+
 
     companion object {
         private const val VIEW_PURCHASE_DETAILS = "%d개를 구매했습니다."
+        private const val VIEW_TOTAL_PROFIT = "총 수익률은 %.1f%%입니다."
     }
 }
